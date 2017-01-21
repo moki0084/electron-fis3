2e616c8ecc47adf617cf9e946e662327057f5607
moki0084 <moki0084@gmail.com>
2017-01-21 16:57:20 +0800

    liveload 与watch 屏蔽, 基础功能 能使

--- app/fis/run.js
+++ app/fis/run.js
@@ -1,9 +1,11 @@
 var fis = require('fis3');
 var argv = require('yargs').argv;
 delete argv['$0'];
+delete argv['child-flag'];
+delete argv['childFlag'];
 
 fis.cli.run(argv, {
-  cwd: argv.r,
+  cwd: argv.r || __dirname,
   require: [],
   configNameSearch: ['fis-conf.js'],
   configPath: __dirname + '\\fis-conf.js',
